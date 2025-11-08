21bc3f32ec214e6150f6a0624ff8e6e254b8c527
hamajj <mehmethamzaakca@tutamail.com>
2025-11-08 17:22:21 +0300

    changed projects command output

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const output = document.getElementById('output');
 const commands = {
     help: "Available commands:<br>about <br>projects <br>contact",
     about: "Hi! My name is Mehmet Hamza Akca.<br>I'm a secondary school student passionate about coding and technology.<br>I'm interested in game development and low-level software engineering.<br>You can check out my projects through the <strong>projects</strong> command.",
-    projects: "Projects:<br>Portfolio: https://github.com/hamajj/portfolio <br>Sorting Visualizer: https://github.com/hamajj/sortingvisualizer <br>teditor: https://github.com/hamajj/teditor",
+    projects: "Projects:<br>Portfolio: https://github.com/hamajj/portfolio <br>Sorting Visualizer: https://github.com/hamajj/sortingvisualizer <br>teditor: https://github.com/hamajj/teditor<br>procman: https://github.com/hamajj/procman<br><br>More projects coming soon!",
     contact: "You can reach me at: mehmethamzaakca@tutamail.com<br>Or find me on GitHub: https://github.com/hamajj"
 };
 
